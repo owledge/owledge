ee69608feaa6aea7268b0cde4d10e2671304569a
HebertCordero <heberto.cordero@outlook.com>
2019-02-08 18:21:55 -0600

    test

--- server.js
+++ server.js
@@ -7,26 +7,26 @@ const path = require('path');
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
 
-const app = express()
-const dev = app.get('env') !== 'production'
+const app = express();
+const dev = app.get('env') !== 'production';
 
 if(!dev) {
-    app.disable('x-powered-by')
-    app.use(compression())
-    app.use(morgan('common'))
+    app.disable('x-powered-by');
+    app.use(compression());
+    app.use(morgan('common'));
 
-    app.use(express.static(path.resolve(__dirname, 'build')))
+    app.use(express.static(path.resolve(__dirname, 'build')));
 
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
 
 if(dev){
-    app.use(morgan('dev'))
+    app.use(morgan('dev'));
 }
 
-const server = createServer(app)
+const server = createServer(app);
 
 server.listen(PORT, err => {
     if(err) throw err;
