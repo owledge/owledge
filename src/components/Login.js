5c76bc6769de1ae733efd45bd3e1a801b025f40d
Almaraz <ivan.almz.r@gmail.com>
2019-03-19 23:41:17 -0600

    Agregado de responsividad en login
    Se agregó responsividad a la pantalla, alineando los elementos al centro.

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,26 +2,29 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
-
+import Logo from '../components/Logo';
 
 
 class Login extends Component {
     render() {
         return (
             <div className="Login">
-                <br/>
-                <Form className="col-md-12">
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="email" placeholder="Ingrese correo electrónico" />
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Control type="password" placeholder="Contraseña" />
-                    </Form.Group>
-                    <Button variant="danger" type="submit" className="Basic-button">
-                        Enviar
+                <Logo />
+                <div className="container">
+                <div className="row justify-content-md-center"> 
+                    <Form className="col col-lg-6">
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="Ingrese correo electrónico" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Contraseña" />
+                        </Form.Group>
+                            <Button variant="danger" type="submit" className="Basic-button btn-lg">
+                            Enviar
                     </Button>
-                </Form>
+                    </Form>
+                </div>
+                </div>
             </div>
         );
     }
