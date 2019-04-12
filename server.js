500fdfe227243445b4cfb1047e0f897e52cc1b5b
Daniel Moreno <daniel10697@hotmail.com>
2019-04-11 19:07:07 -0500

    Revision de conexiones de api con heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(path.resolve(__dirname, 'build')))
 
-  app.get('*', (req, res) =>{
+  app.get('/', (req, res) =>{
       res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   })
 }
