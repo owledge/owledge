f6a4afedf06f7e1857ad7811542b008e7fb14a53
Alan Valdez <valdezalan97@outlook.com>
2019-04-13 16:46:45 -0500

    Added tag_id to Flashcard creation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 const compression = require("compression");
+const util = require('util');
 const morgan = require("morgan");
 var mysql = require("mysql");
 const path = require("path");
@@ -31,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
 if (dev) {
   app.use(morgan("dev"));
 }
-
+const query = util.promisify(connection.query).bind(connection);
 connection.connect(err => {
   if (!err) {
     console.log("Db connection succedes.");
@@ -44,7 +45,9 @@ connection.connect(err => {
 
 app.use(express.static(__dirname));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 // parse application/json
 app.use(bodyParser.json());
 
@@ -52,7 +55,9 @@ app.listen(process.env.PORT || 3000, () => console.log("Port 3000"));
 
 app.use(
   basicAuth({
-    users: { admin: "123" },
+    users: {
+      admin: "123"
+    },
     challenge: true
   })
 );
@@ -63,25 +68,25 @@ app.post("/api/users", (req, res) => {
   //var token = jwt.sign({ foo: post_body.pword }, 'shhhhh')
   connection.query(
     "INSERT into User (uname, fname, lname, pword, gender, email, regdate, country, language) VALUES ('" +
-      post_body.uname +
-      "','" +
-      post_body.fname +
-      "','" +
-      post_body.lname +
-      "','" +
-      post_body.pword +
-      "','" +
-      post_body.gender +
-      "','" +
-      post_body.email +
-      "','" +
-      post_body.regdate +
-      "','" +
-      post_body.country +
-      "','" +
-      post_body.language +
-      "')",
-    function(err, data) {
+    post_body.uname +
+    "','" +
+    post_body.fname +
+    "','" +
+    post_body.lname +
+    "','" +
+    post_body.pword +
+    "','" +
+    post_body.gender +
+    "','" +
+    post_body.email +
+    "','" +
+    post_body.regdate +
+    "','" +
+    post_body.country +
+    "','" +
+    post_body.language +
+    "')",
+    function (err, data) {
       if (err) {
         res.send(err);
       } else {
@@ -109,32 +114,32 @@ app.put("/api/users/:id", (req, res) => {
   var post_body = req.body;
   connection.query(
     "UPDATE User " +
-      "SET uname = '" +
-      post_body.uname +
-      "'," +
-      "fname ='" +
-      post_body.fname +
-      "'," +
-      "lname ='" +
-      post_body.lname +
-      "'," +
-      "pword ='" +
-      post_body.pword +
-      "'," +
-      "gender ='" +
-      post_body.gender +
-      "'," +
-      "email ='" +
-      post_body.email +
-      "'," +
-      "country ='" +
-      post_body.country +
-      "'," +
-      "language ='" +
-      post_body.language +
-      "' WHERE user_id = ?",
+    "SET uname = '" +
+    post_body.uname +
+    "'," +
+    "fname ='" +
+    post_body.fname +
+    "'," +
+    "lname ='" +
+    post_body.lname +
+    "'," +
+    "pword ='" +
+    post_body.pword +
+    "'," +
+    "gender ='" +
+    post_body.gender +
+    "'," +
+    "email ='" +
+    post_body.email +
+    "'," +
+    "country ='" +
+    post_body.country +
+    "'," +
+    "language ='" +
+    post_body.language +
+    "' WHERE user_id = ?",
     [req.params.id],
-    function(err, data) {
+    function (err, data) {
       if (err) {
         res.send(err);
       } else {
@@ -150,11 +155,11 @@ app.post("/api/users/login", (req, res) => {
   var post_body = req.body;
   connection.query(
     "SELECT * FROM User where email='" +
-      post_body.email +
-      "' and pword='" +
-      post_body.pword +
-      "'",
-    function(err, data) {
+    post_body.email +
+    "' and pword='" +
+    post_body.pword +
+    "'",
+    function (err, data) {
       if (err) {
         res.send(err);
       } else {
@@ -167,26 +172,34 @@ app.post("/api/users/login", (req, res) => {
 });
 
 // Post card
-app.post("/api/cards/:userId", (req, res) => {
+app.post("/api/cards/:userId", async (req, res) => {
   var post_body = req.body;
-  connection.query(
-    "INSERT into FlashCard (question, answer, creation_date, owner_id) VALUES ('" +
+  try {
+    const rows = await query("SELECT GetTagId (?) as id", post_body.tag_name);
+    const tag_id = rows[0].id;
+    connection.query(
+      "INSERT into FlashCard (question, answer, creation_date, owner_id, tag_id) VALUES ('" +
       post_body.question +
       "','" +
       post_body.answer +
       "','" +
-      new Date().toISOString() +
+      formatDate() +
       "','" +
       +req.params.userId +
+      "','" +
+      tag_id +
       "')",
-    function(err, data) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(data);
+      function (err, data) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(data);
+        }
       }
-    }
-  );
+    );
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 // Dislike flashcard GET /api/cards/:userId/dislike/:flashcardId?decision=true/false
@@ -197,7 +210,7 @@ app.get("/api/cards/:userId/dislike/:flashcardId", (req, res) => {
     connection.query(
       "UPDATE User_has_FlashCard SET has_disliked = ? WHERE User_id = ? AND FlashCard_id = ?",
       [decision, +req.params.userId, +req.params.flashcardId],
-      function(err, data) {
+      function (err, data) {
         if (err) {
           res.send(err);
         } else {
@@ -216,7 +229,7 @@ app.put("/api/cards/:userId/assign/:flashcardId", (req, res) => {
   connection.query(
     "INSERT INTO User_has_FlashCard(User_id,FlashCard_id) VALUES (?, ?)",
     [+req.params.userId, +req.params.flashcardId],
-    function(err, data) {
+    function (err, data) {
       if (err) {
         res.send(err);
       } else {
@@ -232,7 +245,7 @@ app.get("/api/cards/assigned/:id", (req, res) => {
   connection.query(
     "SELECT FlashCard.*, User_has_FlashCard.has_disliked FROM FlashCard JOIN User_has_FlashCard ON FlashCard.flashcard_id = User_has_FlashCard.FlashCard_id WHERE User_has_FlashCard.User_id = ?",
     [req.params.id],
-    function(err, data) {
+    function (err, data) {
       if (err) {
         res.send(err);
       } else {
@@ -248,7 +261,7 @@ app.get("/api/cards/owned/:id", (req, res) => {
   connection.query(
     "SELECT FlashCard.*, User_has_FlashCard.has_disliked FROM FlashCard JOIN User_has_FlashCard ON FlashCard.flashcard_id = User_has_FlashCard.FlashCard_id WHERE FlashCard.owner_id = ?",
     [req.params.id],
-    function(err, data) {
+    function (err, data) {
       if (err) {
         res.send(err);
       } else {
@@ -263,14 +276,14 @@ app.put("/api/cards/update/:userId&:flashcardId", (req, res) => {
   var post_body = req.body;
   connection.query(
     "UPDATE FlashCard " +
-      "SET question = '" +
-      post_body.question +
-      "'," +
-      "answer ='" +
-      post_body.answer +
-      "' WHERE owner_id = ? AND flashcard_id = ?",
+    "SET question = '" +
+    post_body.question +
+    "'," +
+    "answer ='" +
+    post_body.answer +
+    "' WHERE owner_id = ? AND flashcard_id = ?",
     [req.params.userId, req.params.flashcardId],
-    function(err, data) {
+    function (err, data) {
       if (err) {
         res.send(err);
       } else {
@@ -280,7 +293,7 @@ app.put("/api/cards/update/:userId&:flashcardId", (req, res) => {
   );
 });
 
-// Get flascard por id
+// Get flashcard por id
 app.get("/api/flashcards/:id", (req, res) => {
   connection.query(
     "SELECT * FROM FlashCard WHERE flashcard_id = ?",
@@ -290,82 +303,6 @@ app.get("/api/flashcards/:id", (req, res) => {
         res.send(rows);
       } else {
         console.log(err);
-      }
-    }
-  );
-});
-
-// Post tag
-app.post("/api/tags/", (req, res) => {
-  var post_body = req.body;
-  connection.query(
-    "INSERT into Tag (tag_name, tag_color, tag_icon) VALUES ('" +
-      post_body.tag_name +
-      "','" +
-      post_body.tag_color +
-      "','" +
-      post_body.tag_icon +
-      "')",
-    function(err, data) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-// Put tag
-app.put("/api/tags/:id", (req, res) => {
-  var post_body = req.body;
-  connection.query(
-    "UPDATE Tag " +
-      "SET tag_name = '" +
-      post_body.tag_name +
-      "'," +
-      "tag_color ='" +
-      post_body.tag_color +
-      "'," +
-      "tag_icon ='" +
-      post_body.tag_icon +
-      "' WHERE tag_id = ?",
-    [req.params.id],
-    function(err, data) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-// Get tag por id
-app.get("/api/tags/id/:id", (req, res) => {
-  connection.query(
-    "SELECT * FROM Tag WHERE tag_id = ?",
-    [req.params.id],
-    (err, rows, fields) => {
-      if (!err) {
-        res.send(rows);
-      } else {
-        console.log("err");
-      }
-    }
-  );
-});
-
-// Get tag por nombre
-app.get("/api/tags/name/:name", (req, res) => {
-  connection.query(
-    "SELECT * FROM Tag WHERE tag_name = ?",
-    [req.params.name],
-    (err, rows, fields) => {
-      if (!err) {
-        res.send(rows);
-      } else {
-        console.log("err");
       }
     }
   );
