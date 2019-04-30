7dde7fef159746b5b7ef1b15832b6cdd146e2b1b
Daniel Moreno <daniel10697@hotmail.com>
2019-04-30 17:36:12 -0500

    Agregado de pagina principal

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ class App extends Component {
     return (
       <div className="App">
         < Header />
+        <div class="row welcomePage">
+          <div class="col-12">
+            <h1>Bienvenido a Owledge</h1>
+            <p> Texto que incluira Owledge </p>
+            <button class="btn btn-primary">Ve lo que estudia la comunidad</button>
+          </div>
+        </div>
       </div>
     );
   }
