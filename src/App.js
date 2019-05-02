3a278eccd9d13289cb693e9c0556d259975cc360
Daniel Moreno <daniel10697@hotmail.com>
2019-05-01 19:49:48 -0500

    Agregado de pagina home

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react';
 import logo from './res/logo.svg';
 import './styles/App.css';
 import Header from './components/Header';
+import Home from './components/Home';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         < Header />
-        <div class="row welcomePage">
-          <div class="col-12">
-            <h1>Bienvenido a Owledge</h1>
-            <p> Texto que incluira Owledge </p>
-            <button class="btn btn-primary">Ve lo que estudia la comunidad</button>
-          </div>
-        </div>
+        <BrowserRouter>
+          <Route path="/" component={Home}/>
+        </BrowserRouter>
       </div>
     );
   }
